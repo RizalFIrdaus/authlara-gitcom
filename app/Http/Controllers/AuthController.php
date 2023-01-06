c3e09347831b2376f22b85c44ab3ec8fd2d75133
RizalFIrdaus <rizal.firdaus117@gmail.com>
2023-01-06 16:38:34 +0700

    modified to api response

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'name' => 'required',
+            'password' => 'required'
+        ]);
+
+        $user = User::create([
+            'email' => $request->email,
+            'name' => $request->name,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'data' => $user
+        ]);
+    }
 }
