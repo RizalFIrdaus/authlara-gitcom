d83120f08e8c88fd005576818694e427d389cb57
RizalFIrdaus <rizal.firdaus117@gmail.com>
2023-01-08 12:14:05 +0700

    Commented any code in route folder

--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,14 @@ Route::get('/', function () {
 });
 
 
-Route::middleware(['auth:sanctum', 'verified'])->group(function () {
-    Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
-    Route::get('/nilai/{id}', [HomeController::class, 'edit'])->name('edit.nilai');
-    Route::get('/nilai', [HomeController::class, 'create']);
-    Route::post('/nilai', [HomeController::class, 'store'])->name('edit.nilai');
-    Route::put('/nilai/{id}', [HomeController::class, 'update'])->name('update.nilai');
-    Route::delete('/nilai/{id}', [HomeController::class, 'destroy'])->name('delete.nilai');
-});
+// Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+//     Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
+//     Route::get('/nilai/{id}', [HomeController::class, 'edit'])->name('edit.nilai');
+//     Route::get('/nilai', [HomeController::class, 'create']);
+//     Route::post('/nilai', [HomeController::class, 'store'])->name('edit.nilai');
+//     Route::put('/nilai/{id}', [HomeController::class, 'update'])->name('update.nilai');
+//     Route::delete('/nilai/{id}', [HomeController::class, 'destroy'])->name('delete.nilai');
+// });
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
